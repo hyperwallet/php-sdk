e2ca44b720ba420fda927cfa185457dc67e404d7
Florian Krauthan <fkrauthan@hyperwallet.com>
2016-06-17 10:55:28 -0700

    Added tests for users api methods

--- tests/Hyperwallet/Tests/HyperwalletTest.php
+++ tests/Hyperwallet/Tests/HyperwalletTest.php
@@ -5,6 +5,8 @@ use GuzzleHttp\Client;
 use GuzzleHttp\Psr7\Uri;
 use Hyperwallet\Exception\HyperwalletArgumentException;
 use Hyperwallet\Hyperwallet;
+use Hyperwallet\Model\User;
+use Hyperwallet\Util\ApiClient;
 
 class HyperwalletTest extends \PHPUnit_Framework_TestCase {
 
@@ -36,6 +38,168 @@ class HyperwalletTest extends \PHPUnit_Framework_TestCase {
         $this->validateGuzzleClientSettings($client, 'https://test.test', 'test-username', 'test-password');
     }
 
+    //--------------------------------------
+    // Users
+    //--------------------------------------
+
+    public function testCreateUser_withoutProgramToken() {
+        // Setup
+        $client = new Hyperwallet('test-username', 'test-password');
+        $apiClientMock = $this->createAndInjectApiClientMock($client);
+        $user = new User();
+
+        \Phake::when($apiClientMock)->doPost('/rest/v3/users', array(), $user, array())->thenReturn(array('success' => 'true'));
+
+        // Run test
+        $this->assertNull($user->getProgramToken());
+
+        $newUser = $client->createUser($user);
+        $this->assertNotNull($newUser);
+        $this->assertNull($user->getProgramToken());
+        $this->assertEquals(array('success' => 'true'), $newUser->getProperties());
+
+        // Validate mock
+        \Phake::verify($apiClientMock)->doPost('/rest/v3/users', array(), $user, array());
+    }
+
+    public function testCreateUser_withProgramTokenAddedByDefault() {
+        // Setup
+        $client = new Hyperwallet('test-username', 'test-password', 'test-program-token');
+        $apiClientMock = $this->createAndInjectApiClientMock($client);
+        $user = new User();
+
+        \Phake::when($apiClientMock)->doPost('/rest/v3/users', array(), $user, array())->thenReturn(array('success' => 'true'));
+
+        // Run test
+        $this->assertNull($user->getProgramToken());
+
+        $newUser = $client->createUser($user);
+        $this->assertNotNull($newUser);
+        $this->assertEquals('test-program-token', $user->getProgramToken());
+        $this->assertEquals(array('success' => 'true'), $newUser->getProperties());
+
+        // Validate mock
+        \Phake::verify($apiClientMock)->doPost('/rest/v3/users', array(), $user, array());
+    }
+
+    public function testCreateUser_withProgramTokenInUserObject() {
+        // Setup
+        $client = new Hyperwallet('test-username', 'test-password', 'test-program-token');
+        $apiClientMock = $this->createAndInjectApiClientMock($client);
+        $user = new User(array('programToken' => 'test-program-token2'));
+
+        \Phake::when($apiClientMock)->doPost('/rest/v3/users', array(), $user, array())->thenReturn(array('success' => 'true'));
+
+        // Run test
+        $this->assertEquals('test-program-token2', $user->getProgramToken());
+
+        $newUser = $client->createUser($user);
+        $this->assertNotNull($newUser);
+        $this->assertEquals('test-program-token2', $user->getProgramToken());
+        $this->assertEquals(array('success' => 'true'), $newUser->getProperties());
+
+        // Validate mock
+        \Phake::verify($apiClientMock)->doPost('/rest/v3/users', array(), $user, array());
+    }
+
+    public function testGetUser_noUserToken() {
+        // Setup
+        $client = new Hyperwallet('test-username', 'test-password');
+
+        try {
+            $client->getUser('');
+            $this->fail('HyperwalletArgumentException expected');
+        } catch (HyperwalletArgumentException $e) {
+            $this->assertEquals('userToken is required!', $e->getMessage());
+        }
+    }
+
+    public function testGetUser_allParameters() {
+        // Setup
+        $client = new Hyperwallet('test-username', 'test-password', 'test-program-token');
+        $apiClientMock = $this->createAndInjectApiClientMock($client);
+
+        \Phake::when($apiClientMock)->doGet('/rest/v3/users/{user-token}', array('user-token' => 'test-user-token'), array())->thenReturn(array('success' => 'true'));
+
+        // Run test
+        $user = $client->getUser('test-user-token');
+        $this->assertNotNull($user);
+        $this->assertEquals(array('success' => 'true'), $user->getProperties());
+
+        // Validate mock
+        \Phake::verify($apiClientMock)->doGet('/rest/v3/users/{user-token}', array('user-token' => 'test-user-token'), array());
+    }
+
+    public function testUpdateUser_noToken() {
+        // Setup
+        $client = new Hyperwallet('test-username', 'test-password');
+        $user = new User();
+
+        try {
+            $client->updateUser($user);
+            $this->fail('HyperwalletArgumentException expected');
+        } catch (HyperwalletArgumentException $e) {
+            $this->assertEquals('token is required!', $e->getMessage());
+        }
+    }
+
+    public function testUpdateUser_allParameters() {
+        // Setup
+        $client = new Hyperwallet('test-username', 'test-password');
+        $apiClientMock = $this->createAndInjectApiClientMock($client);
+        $user = new User(array('token' => 'test-user-token'));
+
+        \Phake::when($apiClientMock)->doPut('/rest/v3/users/{user-token}', array('user-token' => 'test-user-token'), $user, array())->thenReturn(array('success' => 'true'));
+
+        // Run test
+        $newUser = $client->updateUser($user);
+        $this->assertNotNull($newUser);
+        $this->assertEquals(array('success' => 'true'), $newUser->getProperties());
+
+        // Validate mock
+        \Phake::verify($apiClientMock)->doPut('/rest/v3/users/{user-token}', array('user-token' => 'test-user-token'), $user, array());
+    }
+
+    public function testListUsers_noParameters() {
+        // Setup
+        $client = new Hyperwallet('test-username', 'test-password', 'test-program-token');
+        $apiClientMock = $this->createAndInjectApiClientMock($client);
+
+        \Phake::when($apiClientMock)->doGet('/rest/v3/users', array(), array())->thenReturn(array('count' => 1, 'data' => array()));
+
+        // Run test
+        $userList = $client->listUsers();
+        $this->assertNotNull($userList);
+        $this->assertCount(0, $userList);
+        $this->assertEquals(1, $userList->getCount());
+
+        // Validate mock
+        \Phake::verify($apiClientMock)->doGet('/rest/v3/users', array(), array());
+    }
+
+    public function testListUsers_withParameters() {
+        // Setup
+        $client = new Hyperwallet('test-username', 'test-password', 'test-program-token');
+        $apiClientMock = $this->createAndInjectApiClientMock($client);
+
+        \Phake::when($apiClientMock)->doGet('/rest/v3/users', array(), array('test' => 'value'))->thenReturn(array('count' => 1, 'data' => array(array('success' => 'true'))));
+
+        // Run test
+        $userList = $client->listUsers(array('test' => 'value'));
+        $this->assertNotNull($userList);
+        $this->assertCount(1, $userList);
+        $this->assertEquals(1, $userList->getCount());
+
+        $this->assertEquals(array('success' => 'true'), $userList[0]->getProperties());
+
+        // Validate mock
+        \Phake::verify($apiClientMock)->doGet('/rest/v3/users', array(), array('test' => 'value'));
+    }
+
+    //--------------------------------------
+    // Internal utils
+    //--------------------------------------
+
     private function validateGuzzleClientSettings(Hyperwallet $client, $server, $username, $password) {
         $clientClazz = new \ReflectionObject($client);
         $apiClientProperty = $clientClazz->getProperty('client');
@@ -52,6 +216,19 @@ class HyperwalletTest extends \PHPUnit_Framework_TestCase {
 
         $this->assertEquals(new Uri($server), $guzzleClient->getConfig('base_uri'));
         $this->assertEquals(array($username, $password), $guzzleClient->getConfig('auth'));
+    }
+
+    private function createAndInjectApiClientMock(Hyperwallet $client) {
+        /** @var ApiClient $apiClientMock */
+        $apiClientMock = \Phake::mock('Hyperwallet\Util\ApiClient');
+
+        $clientClazz = new \ReflectionObject($client);
+        $apiClientProperty = $clientClazz->getProperty('client');
+
+        $apiClientProperty->setAccessible(true);
+        $apiClientProperty->setValue($client, $apiClientMock);
+
+        return $apiClientMock;
     }
 
 }
