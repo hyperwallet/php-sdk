be328de8e10d192bbb2381e2ba7c6e70e68c68b7
Florian Krauthan <fkrauthan@hyperwallet.com>
2016-06-20 15:19:09 -0700

    Added a redirect index.html

--- scripts/generateDocs.sh
+++ scripts/generateDocs.sh
@@ -5,3 +5,21 @@ if [ ! -f ./sami.phar ]; then
 fi
 
 php ./sami.phar update ./sami.cfg.php
+
+echo "
+    <!DOCTYPE HTML>
+    <html lang="en-US">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="refresh" content="1;url=/master">
+            <script type="text/javascript">
+                window.location.href = "/master";
+            </script>
+            <title>Page Redirection</title>
+        </head>
+        <body>
+            <!-- Note: don't tell people to `click` the link, just tell them that it is a link. -->
+            If you are not redirected automatically, follow the <a href='/master'>link to SDK documentation</a>
+        </body>
+    </html>
+" >> ./docs/index.html
