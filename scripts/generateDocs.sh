a23f996bc61a19a81d911e3998bface55578a6cd
Florian Krauthan <fkrauthan@hyperwallet.com>
2016-06-20 15:22:35 -0700

    Finalized the redirect html template

--- scripts/generateDocs.sh
+++ scripts/generateDocs.sh
@@ -6,20 +6,18 @@ fi
 
 php ./sami.phar update ./sami.cfg.php
 
-echo "
-    <!DOCTYPE HTML>
-    <html lang="en-US">
-        <head>
-            <meta charset="UTF-8">
-            <meta http-equiv="refresh" content="1;url=/master">
-            <script type="text/javascript">
-                window.location.href = "/master";
-            </script>
-            <title>Page Redirection</title>
-        </head>
-        <body>
-            <!-- Note: don't tell people to `click` the link, just tell them that it is a link. -->
-            If you are not redirected automatically, follow the <a href='/master'>link to SDK documentation</a>
-        </body>
-    </html>
-" >> ./docs/index.html
+echo "<!DOCTYPE HTML>
+<html lang=\"en-US\">
+    <head>
+        <meta charset=\"UTF-8\">
+        <meta http-equiv="refresh" content=\"1;url=./master\">
+        <script type=\"text/javascript\">
+            window.location.href = \"./master\";
+        </script>
+        <title>Page Redirection</title>
+    </head>
+    <body>
+        If you are not redirected automatically, follow the <a href=\"./master\">link to SDK documentation</a>
+    </body>
+</html>
+" > ./docs/index.html
