07468f3eb4a6a0a8db02d63db17d470e6cae10eb
Pavel Moravek <pmoravek@hyperwallet.com>
2018-10-19 11:12:41 -0700

    Update RsaOaep256AlgorithmInstaller.php
    Added doc

--- src/ComposerScript/RsaOaep256AlgorithmInstaller.php
+++ src/ComposerScript/RsaOaep256AlgorithmInstaller.php
@@ -4,6 +4,11 @@ namespace ComposerScript;
 
 use Composer\Script\Event;
 
+/**
+ * RsaOaep256AlgorithmInstaller copies JWE.php file with RSA-OAEP-256 configuration to the gree/jose library, so it can support RSA-OAEP-256 algorithm
+ * 
+ * gree/jose library is missing content encryption algorithm RSA-OAEP-256 which is mandatory for encrypted REST communication with hyperwallet app. 
+ */
 class RsaOaep256AlgorithmInstaller {
 
     public static function install(Event $event) {
