b8203b87aaf83c9427710e7707fe7a90e6cbeac0
jkurra-hw <jkurra@paypal.com>
2020-09-21 21:04:45 +0530

    HW-66357-V3PHP-VenmoAccount

---
+++ src/Hyperwallet/Model/VenmoAccountStatusTransition.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace Hyperwallet\Model;
+
+/**
+ * Represents a V4 Venmo Account Status Transition
+ *
+ * @package Hyperwallet\Model
+ */
+class VenmoAccountStatusTransition extends StatusTransition
+{
+    const TRANSITION_DE_ACTIVATED = 'DE_ACTIVATED';
+
+    /**
+     * Creates a instance of VenmoAccountStatusTransition
+     *
+     * @param string[] $properties The default properties
+     */
+    public function __construct(array $properties = array())
+    {
+        parent::__construct($properties);
+    }
+
+}
