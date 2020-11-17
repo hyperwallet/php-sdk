604dbee268bc5feb594cf64bda28971ad232cb99
Rajendran Rathinasabapathy <rrathinasabapath@paypal.com>
2020-11-17 21:47:24 +0530

    Added test cases after changes for adding custom headers

--- tests/Hyperwallet/Tests/HyperwalletTest.php
+++ tests/Hyperwallet/Tests/HyperwalletTest.php
@@ -4718,7 +4718,7 @@ class HyperwalletTest extends \PHPUnit_Framework_TestCase {
     /*Following are the methods for integration testing. Please change $includeIntegrationTest to True if you want
     *the following test cases
     */
-
+/*
     public function testGetUserSampleIT() {
         $username = "selrestuser@1861681";
         $password = "Password1!";
@@ -4737,5 +4737,50 @@ class HyperwalletTest extends \PHPUnit_Framework_TestCase {
             echo $e->getMessage();
             die("\n");
         }
+    }
+*/
+    public function testCreateUser_successfulIT() {
+        $username = "selrestuser@1861681";
+        $password = "Password1!";
+        $programToken = "prg-eedaf875-01f1-4524-8b94-d4936255af78";
+        $server = "https://localhost-hyperwallet.aws.paylution.net:8181";
+        $userToken = "usr-f49967a9-9b7f-4cfc-9fc7-037d736711ba";
+
+        $hyperwallet = new \Hyperwallet\Hyperwallet($username, $password, $programToken, $server);
+        $user = $hyperwallet->getUser($userToken);
+        var_dump('User details', $user);
+
+        $user = new User(array(  "clientUserId"=> "user1877253",
+            "email"=> "user1877253@sink.sendgrid.net",
+            "profileType"=> "INDIVIDUAL",
+            "firstName"=> "Stan",
+            "middleName"=> "QA",
+            "lastName"=> "Fung",
+            "dateOfBirth"=> "1980-01-01",
+            "phoneNumber"=> "647-90531",
+            "addressLine1"=> "abc",
+            "city"=> "vancouver",
+            "gender"=> "MALE",
+            "stateProvince"=> "AZ",
+            "country"=> "US",
+            "postalCode"=> "12345",
+            "language"=> "en",
+            "mobileNumber"=> "605-555-1323",
+            "timeZone"=> "MST",
+            "programToken"=> $programToken,
+            "governmentId"=> "987654321",
+            "verificationStatus"=>"NOT_REQUIRED",
+            "businessStakeholderVerificationStatus"=>"VERIFIED",
+            "letterOfAuthorizationStatus"=>"VERIFIED",
+            "governmentIdType"=>"NATIONAL_ID_CARD"
+        ));
+        // Run test
+        if (!$this->includeIntegrationTest) {
+            $this->markTestSkipped('This test is skipped.');
+        }
+        $newStatusTransition = $hyperwallet->createUser($user);
+        var_dump('--------New user------',$user);
+        $this->assertNotNull($newStatusTransition);
+        $this->assertEquals('NOT_REQUIRED', $newStatusTransition->getVerificationStatus());
     }
 }
