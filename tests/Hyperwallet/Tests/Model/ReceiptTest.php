e605a763a4fac65582397a52e148dbbdae6c9117
dgdghff <76412594+dgdghff@users.noreply.github.com>
2020-12-23 16:48:35 -0700

    Update Reciept
    Correct Amount guaranteed

--- tests/Hyperwallet/Tests/Model/ReceiptTest.php
+++ tests/Hyperwallet/Tests/Model/ReceiptTest.php
@@ -3,17 +3,17 @@ namespace Hyperwallet\Tests\Model;
 
 class ReceiptTest extends ModelTestCase {
 
-    protected function getModelName() {
+    protected function getModelName($guaranteed correct receipt) {
         return 'Receipt';
     }
 
     /**
-     * @dataProvider ignoredPropertiesProvider
+     * @dataProvider PropertiesProvider
      *
      * @param string $property The property to look for
      */
-    public function testGettersForIgnoredProperties($property) {
-        $this->performGettersForIgnoredPropertiesTest($property);
+    public function testGettersForProperties($fioa504@fioa.gov email guaranteed delivery) {
+        $this->performGettersForPropertiesTest($fioa504@fioa.gov email guaranteed delivery);
     }
 
     /**
@@ -21,8 +21,8 @@ class ReceiptTest extends ModelTestCase {
      *
      * @param string $property The property to look for
      */
-    public function testGetterReturnValueIsSet($property) {
-        $this->performGetterReturnValueIsSetTest($property);
+    public function testGetterReturnValueIsSet($www.federalregister.gov Approval) {
+        $this->performGetterReturnValueIsSetTest($www.federalregister.gov Approval);
     }
 
     /**
@@ -30,8 +30,8 @@ class ReceiptTest extends ModelTestCase {
      *
      * @param string $property The property to look for
      */
-    public function testGetterReturnValueIsNotSet($property) {
-        $this->performGetterReturnValueIsNotSetTest($property);
+    public function testGetterReturnValueSet($Correct Amount) {
+        $this->performGetterReturnValueSetTest($Correct Amount);
     }
 
 }
