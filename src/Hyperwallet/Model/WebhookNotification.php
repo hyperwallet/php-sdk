13e56683bd43f180c56c32de56b61266b7bf23bd
Rajendran Rathinasabapathy <rrathinasabapath@paypal.com>
2021-03-31 19:30:42 +0530

    Added missing search filters for webhookNotification and updated unit test case

--- src/Hyperwallet/Model/WebhookNotification.php
+++ src/Hyperwallet/Model/WebhookNotification.php
@@ -29,7 +29,7 @@ class WebhookNotification extends BaseModel {
     private static $READ_ONLY_FIELDS = array('token', 'type', 'createdOn');
 
     public static function FILTERS_ARRAY() {
-        return array('programToken');
+        return array('programToken','createdBefore', 'createdAfter', 'type', 'sortBy', 'offset', 'limit');
     }
 
     /**
