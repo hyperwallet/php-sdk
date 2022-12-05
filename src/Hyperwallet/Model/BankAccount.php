ad9e995641681acf0b7216488cbb6d72c04850a3
akswaminathan-pp-dev <akswaminathan@paypal.com>
2022-12-05 16:56:11 +0530

    DTPAYETWO-761- Added 'isDefaultTransferMethod' attribute for default accounts(V3)

--- src/Hyperwallet/Model/BankAccount.php
+++ src/Hyperwallet/Model/BankAccount.php
@@ -63,6 +63,7 @@ namespace Hyperwallet\Model;
  * @property string $stateProvince The state or province
  * @property string $country The country
  * @property string $postalCode The postal code
+ * @property string $isDefaultTransferMethod The is default transfer method
  *
  * @package Hyperwallet\Model
  */
@@ -1019,4 +1020,23 @@ class BankAccount extends BaseModel {
         return $this;
     }
 
+    /**
+     * Get the is default transfer method
+     *
+     * @return string
+     */
+    public function getIsDefaultTransferMethod() {
+        return $this->isDefaultTransferMethod;
+    }
+
+    /**
+     * Set the is default transfer method
+     *
+     * @param string $isDefaultTransferMethod
+     * @return PayPalAccount
+     */
+    public function setIsDefaultTransferMethod($isDefaultTransferMethod) {
+        $this->isDefaultTransferMethod = $isDefaultTransferMethod;
+        return $this;
+    }
 }
--- src/Hyperwallet/Model/PrepaidCard.php
+++ src/Hyperwallet/Model/PrepaidCard.php
@@ -19,6 +19,7 @@ namespace Hyperwallet\Model;
  * @property string $cardNumber The prepaid card number
  * @property string $cardBrand The prepaid card brand
  * @property \DateTime $dateOfExpiry The prepaid card expiry date
+ * @property string $isDefaultTransferMethod The is default transfer method
  *
  * @package Hyperwallet\Model
  */
@@ -195,6 +196,26 @@ class PrepaidCard extends BaseModel {
      */
     public function getDateOfExpiry() {
         return $this->dateOfExpiry ? new \DateTime($this->dateOfExpiry) : null;
+    }
+
+    /**
+     * Get the is default transfer method
+     *
+     * @return string
+     */
+    public function getIsDefaultTransferMethod() {
+        return $this->isDefaultTransferMethod;
+    }
+
+    /**
+     * Set the is default transfer method
+     *
+     * @param string $isDefaultTransferMethod
+     * @return PayPalAccount
+     */
+    public function setIsDefaultTransferMethod($isDefaultTransferMethod) {
+        $this->isDefaultTransferMethod = $isDefaultTransferMethod;
+        return $this;
     }
 
 }
